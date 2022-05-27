4a723bd3d440f8e93cc581458af43b59e5feff40
doodcinki <doodcinki@gmail.com>
2022-05-27 13:04:47 +0100

    Add project files.

---
+++ helloWorld/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace helloWorld
+
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+    }
+}
